e7ae8d668ec3b8c50d5e2556a8ddf0a9e5c5cc25
Nolan Byrne <nolan.byrne@comcast.net>
2020-09-12 19:33:48 -0500

    Added more info to model unit tests to see more info about the model

--- NN/test/small_model_tests.py
+++ NN/test/small_model_tests.py
@@ -9,37 +9,55 @@ class SmallModelTests(unittest.TestCase):
     def test_cold3_less_than_cold2(self):
         cold3 = Deck([4, 94, 19, 64, 65, 52, 43, 78, 37, 20])
         cold2 = Deck([4, 94, 17, 64, 65, 52, 43, 78, 23, 20])
-
-        self.assertLess(self.model.predict(cold3), self.model.predict(cold2))
+        cold3prediction = self.model.predict(cold3)
+        cold2prediction = self.model.predict(cold2)
+        print(cold2prediction - cold3prediction)
+        self.assertLess(cold3prediction, cold2prediction)
 
     def test_cold2_less_than_cold1(self):
         cold2 = Deck([4, 94, 17, 64, 65, 52, 43, 78, 23, 20])
         cold1 = Deck([4, 94, 17, 44, 65, 52, 42, 78, 23, 20])
 
-        self.assertLess(self.model.predict(cold2), self.model.predict(cold1))
+        cold2prediction = self.model.predict(cold2)
+        cold1prediction = self.model.predict(cold1)
+        print(cold1prediction - cold2prediction)
+        self.assertLess(cold2prediction, cold1prediction)
 
     def test_cold1_less_than_neutral(self):
         cold1 = Deck([4, 94, 17, 44, 65, 52, 42, 78, 23, 20])
         neutral = Deck([1, 94, 17, 44, 65, 52, 42, 76, 23, 20])
 
-        self.assertLess(self.model.predict(cold1), self.model.predict(neutral))
+        cold1prediction = self.model.predict(cold1)
+        neutralPrediction = self.model.predict(neutral)
+        print(neutralPrediction - cold1prediction)
+        self.assertLess(cold1prediction, neutralPrediction)
 
     def test_neutral_less_than_hot1(self):
         neutral = Deck([1, 94, 17, 44, 65, 52, 42, 76, 23, 20])
         hot1 = Deck([1, 94, 16, 43, 65, 67, 42, 62, 24, 20])
-        self.assertLess(self.model.predict(neutral), self.model.predict(hot1))
+
+        neutralPrediction = self.model.predict(neutral)
+        hot1prediction = self.model.predict(hot1)
+        print(hot1prediction - neutralPrediction)
+        self.assertLess(neutralPrediction, hot1prediction)
 
     def test_hot1_less_than_hot2(self):
         hot1 = Deck([1, 94, 16, 43, 65, 67, 42, 62, 24, 20])
         hot2 = Deck([53, 99, 16, 43, 65, 67, 42, 62, 24, 20])
 
-        self.assertLess(self.model.predict(hot1), self.model.predict(hot2))
+        hot1prediction = self.model.predict(hot1)
+        hot2prediction = self.model.predict(hot2)
+        print(hot2prediction - hot1prediction)
+        self.assertLess(hot1prediction, hot2prediction)
 
     def test_hot2_less_than_hot3(self):
         hot2 = Deck([53, 99, 16, 43, 65, 67, 42, 62, 24, 20])
         hot3 = Deck([53, 99, 16, 43, 61, 67, 42, 62, 24, 20])
 
-        self.assertLess(self.model.predict(hot2), self.model.predict(hot3))
+        hot2prediction = self.model.predict(hot2)
+        hot3prediction = self.model.predict(hot3)
+        print(hot3prediction - hot2prediction)
+        self.assertLess(hot2prediction, hot3prediction)
 
     @classmethod
     def setUpClass(cls):
